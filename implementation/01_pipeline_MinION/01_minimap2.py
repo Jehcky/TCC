d97ad50c820ac1b6907ca4b2c5c24763574e4fdc
Jessica Teixeira <jehck.tnj@gmail.com>
2023-09-01 01:50:22 +0000

    Início da implementação da execução do pipeline MinION

---
+++ implementation/01_pipeline_MinION/01_minimap2.py
@@ -0,0 +1,23 @@
+import subprocess
+
+# minimap2 command
+#-ax map-ont $REF_FILE $FASTQ > ${SEQ}.PrePilon.sam
+minimap2_command = [
+"minimap2"
+, 
+"-ax"
+, 
+"map-ont"
+, 
+"sequencia_referencia.fasta"
+, 
+"sequencia_query.fastq",
+
+"> ${SEQ}.PrePilon.sam"
+]
+
+# Execute minimap2 and get the output
+output_minimap2 = subprocess.check_output(minimap2_command, universal_newlines=True)
+
+# Exibir a saída do minimap2
+print(output_minimap2)
