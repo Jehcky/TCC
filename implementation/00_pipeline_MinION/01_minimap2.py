b34f76aebbfe6d31b6bad250c4c97a512ca57325
Jéssica Teixeira <jehck.tnj@gmail.com>
2023-09-02 21:42:33 +0000

    Adjustments

--- implementation/00_pipeline_MinION/01_minimap2.py
+++ implementation/00_pipeline_MinION/01_minimap2.py
@@ -1,23 +1,15 @@
 import subprocess
-#import pysam
+import pysam
 
 # minimap2 command
 # minimap2 -ax map-ont $REF_FILE $FASTQ > ${SEQ}.PrePilon.sam
-minimap2_command = [
-"minimap2"
-,
-"-ax"
-,
-"map-ont"
-,
-"/home/TCC/implementation/RefSeq/RefSeq_RHD.fasta"
-,
-"/home/TCC/dataset/FASTQ/uncompressed/AF2RHD_S55_L001_R2_001.fastq"
-,
-">"
-,
-"AF2RHD_S55_L001_R1_001.PrePilon.sam"
-]
+minimap2_command = ["minimap2",
+                    "-ax",
+                    "map-ont",
+                    "/home/TCC/implementation/RefSeq/RefSeq_RHD.fasta",
+                    "/home/TCC/dataset/FASTQ/uncompressed/AF2RHD_S55_L001_R2_001.fastq",
+                    ">",
+                    "AF2RHD_S55_L001_R1_001.PrePilon.sam"]
 
 # Execute minimap2 and get the output
 output_minimap2 = subprocess.check_output(minimap2_command, universal_newlines=True)
