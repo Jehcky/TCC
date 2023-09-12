d2b4c292016ef85b6572f40b13a05333c93cea52
Jéssica Teixeira <jehck.tnj@gmail.com>
2023-09-12 11:15:41 +0000

    Samtools view

--- implementation/00_pipeline_MinION/01_minimap2_01_mapont.py
+++ implementation/00_pipeline_MinION/01_minimap2_01_mapont.py
@@ -1,9 +1,14 @@
 import subprocess
 import os
 
+# minimap2 -ax map-ont $REF_FILE $FASTQ > ${SEQ}.PrePilon.sam
+
 INPUT_FOLDER = "/home/TCC/dataset/FASTQ/uncompressed/"
-OUTPUT_FOLDER = "/home/TCC/result/01_minimap2_result/"
+OUTPUT_FOLDER = "/home/TCC/result/01_minimap2_01_result/"
 REF_SEQ_FOLDER = "/home/TCC/implementation/RefSeq/"
+
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
 
 fastq_files = [f for f in os.listdir(INPUT_FOLDER) if f.endswith(".fastq")]
 
@@ -13,4 +18,4 @@ for fastq_file in fastq_files:
 
     status = subprocess.call(command, shell=True)
     if status:
-        print(f"Alignment complete. .SAM file saved as {fastq_name}.PrePilon.sam.")
+        print(f"Minimap2 map-ont complete. .SAM file saved as {fastq_name}.PrePilon.sam.")
--- implementation/00_pipeline_MinION/02_samtools_01_view.py
+++ implementation/00_pipeline_MinION/02_samtools_01_view.py
@@ -1,2 +1,20 @@
-# View
+import subprocess
+import os
+
 # samtools view -S -b ${SEQ}.PrePilon.sam > ${SEQ}.PrePilon.bam
+
+INPUT_FOLDER = "/home/TCC/result/01_minimap2_01_result/"
+OUTPUT_FOLDER = "/home/TCC/result/02_samtools_01_view_result/"
+
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
+
+sam_files = [f for f in os.listdir(INPUT_FOLDER) if f.endswith(".PrePilon.sam")]
+
+for sam_file in sam_files:
+    sam_name = sam_file.rstrip(".PrePilon.sam")
+    command = f"samtools view -S -b {INPUT_FOLDER}{sam_name}.PrePilon.sam > {OUTPUT_FOLDER}{sam_name}.PrePilon.bam"
+
+    status = subprocess.call(command, shell=True)
+    if status:
+        print(f"Samtools view complete. .BAM file saved as {sam_name}.PrePilon.bam.")
