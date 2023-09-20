7a9a9e6d3441e65921f015a8ba1017ff48209280
Jéssica Teixeira <jehck.tnj@gmail.com>
2023-09-20 22:49:38 +0000

    Changes in MinION pipeline

--- implementation/00_pipeline_MinION/00_uncompress.py
+++ implementation/00_pipeline_MinION/00_uncompress.py
@@ -3,17 +3,19 @@ import gzip
 import shutil
 
 # Pasta onde os arquivos .fastq.gz estão localizados
-INPUT_FOLDER = "/data/TCC/dataset/FASTQ"
-OUTPUT_FOLDER = "/data/TCC/dataset/FASTQ/uncompressed"
+INPUT_FOLDER = "/home/TCC/dataset/FASTQ"
+OUTPUT_FOLDER = "/home/TCC/dataset/FASTQ/uncompressed"
 
 # Verificar se a pasta de saída existe; se não existir, ela é criada
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
 
 # Listar todos os arquivos na pasta de entrada que possuem a extensão .fastq.gz
+print("Searching files...")
 fastq_gz_files = [f for f in os.listdir(INPUT_FOLDER) if f.endswith(".fastq.gz")]
 
 # Loop através de cada arquivo .fastq.gz encontrado
+print("Uncompressing files...")
 for fastq_gz_file in fastq_gz_files:
     # Constrir o caminho completo para o arquivo .fastq.gz de entrada
     fastq_gz_complete_path = os.path.join(INPUT_FOLDER, fastq_gz_file)
@@ -26,4 +28,4 @@ for fastq_gz_file in fastq_gz_files:
         with open(complete_output_path, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-print("Descompressão concluída!")
+print("All files uncompressed!")
